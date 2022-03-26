f9cd174a4c626e64715ba6f015dd5209526d3a38
joao <jgsspinelli92@gmail.com>
2022-03-26 16:29:02 -0300

    [UPDATE] Cria as rotas e as paginas Home e Login

---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home (_props) {
+  return (
+    <h1>This is the Home</h1>
+  )
+}
+
+export default Home;
---
+++ front/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Login (_props) {
+  return (
+    <div>
+      <h1>Login NotePlus</h1>
+      <form action="">
+        <input type="text" placeholder='E-mail...'/>
+        <input type="password" placeholder='Senha...' />
+      </form>
+    </div>
+  )
+}
+
+export default Login;
