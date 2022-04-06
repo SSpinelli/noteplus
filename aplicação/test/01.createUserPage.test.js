ed8da2922d1c605450f36f78b8d3482f136949d6
joao <jgsspinelli92@gmail.com>
2022-04-06 10:29:46 -0300

    catching up

---
+++ aplicação/test/01.createUserPage.test.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+// const resolution = require('../src/pages/CreateUser')
+
+const foo = 2;
+const bar = 2;
+
+describe('sjdiasjdiasj', () => {
+  it('sdds', () => {
+    expect(foo).equal(bar)
+  });
+});
